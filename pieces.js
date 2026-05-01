56f24a1a0ad7cb6633291b4751c44d9945870651
neilthornock <neilthornock@gmail.com>
2026-05-01 17:00:29 -0600

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -1,5 +1,12 @@
 const pieces = {
 
+spring: {
+  title: "Spring (album)",
+  category: "other",
+  instrumentation: "Electronic",
+  youtube: https://www.youtube.com/watch?v=TDxS5f_jAQA
+},
+
 trag: {
   title: "Trag",
   category: "piano",
