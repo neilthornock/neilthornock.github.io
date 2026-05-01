b4b7b26fe97e94ddc3c74d17bfff6f466c28c6c6
neilthornock <neilthornock@gmail.com>
2026-04-30 18:52:31 -0600

    Create pieces.js

---
+++ pieces.js
@@ -0,0 +1,381 @@
+const pieces = {
+
+campanarium1: {
+  title: "Campanarium 1",
+  score: "scores/thornock_neil_campanarium1.pdf"
+},
+
+campanarium2: {
+  title: "Campanarium 2",
+  score: "scores/thornock_neil_campanarium2.pdf"
+},
+
+campanarium3: {
+  title: "Campanarium 3",
+  score: "scores/thornock_neil_campanarium3.pdf"
+},
+
+carillonbooklet: {
+  title: "Carillon Booklet",
+  movements: ["Five numbered movements"],
+  score: "scores/thornock_neil_carillon_booklet.pdf"
+},
+
+ceremonialpreludes: {
+  title: "Ceremonial Preludes",
+  movements: ["Six numbered movements"],
+  score: "scores/thornock_neil_ceremonial_preludes.pdf"
+},
+
+cleardance: {
+  title: "Clear Dance",
+  score: "scores/thornock_neil_cleardance.pdf"
+},
+
+construction: {
+  title: "Construction",
+  score: "scores/thornock_neil_construction.pdf"
+},
+
+dandelionslowdrag: {
+  title: "Dandelion Slow Drag",
+  score: "scores/thornock_neil_dandelionslowdrag.pdf"
+},
+
+dawnwalk: {
+  title: "Dawn Walk",
+  score: "scores/thornock_neil_dawnwalk.pdf"
+},
+
+duskdescending: {
+  title: "Dusk Descending",
+  score: "scores/thornock_neil_dusk_descending.pdf"
+},
+
+fantasia: {
+  title: "Fantasia",
+  score: "scores/thornock_neil_fantasia.pdf"
+},
+
+fantasy: {
+  title: "Fantasy",
+  score: "scores/thornock_neil_fantasy.pdf"
+},
+
+fantasyonenglishfolktunesno1: {
+  title: "Fantasy on English Folk Tunes No. 1",
+  movements: [
+    "Greensleeves",
+    "My Robin Is to the Greenwood Gone",
+    "Oh Waly, Waly, Gin Love Be Bonny",
+    "Lovely Joan"
+  ],
+  score: "scores/thornock_neil_fantasyonenglishfolktunes1.pdf"
+},
+
+fastdances: {
+  title: "Fast Dances",
+  movements: ["Four dances"],
+  score: "scores/thornock_neil_fastdances.pdf"
+},
+
+festivalprelude: {
+  title: "Festival Prelude",
+  score: "scores/thornock_neil_festival-prelude.pdf"
+},
+
+fivefantasiesonromaniancarols: {
+  title: "Five Fantasies on Romanian Carols",
+  movements: [
+    "Doamne Isuse Hristoase",
+    "Domnulet si Domn din Cer",
+    "Ia sculati, sculati",
+    "Colindul Vanatorilor",
+    "Intreaba, Intreaba"
+  ],
+  score: "scores/thornock_neil_five_fantasies_on_romanian_carols.pdf"
+},
+
+halfsteptwostep: {
+  title: "Half Step Two Step",
+  score: "scores/thornock_neil_half_step_two_step.pdf"
+},
+
+hypergoxary: {
+  title: "Hypergoxary",
+  score: "scores/thornock_neil_hypergoxary.pdf"
+},
+
+impressions: {
+  title: "Impressions",
+  movements: [
+    "Fleeting",
+    "Turquoise Empress",
+    "The Snow Never Fell",
+    "Water Spell"
+  ],
+  score: "scores/thornock_neil_impressions.pdf"
+},
+
+lavendershroud: {
+  title: "Lavender Shroud",
+  score: "scores/thornock_neil_lavender_shroud.pdf"
+},
+
+litany: {
+  title: "Litany",
+  score: "scores/thornock_neil_litany.pdf"
+},
+
+litanychaconne: {
+  title: "Litany-Chaconne",
+  score: "scores/thornock_neil_litanychaconne.pdf"
+},
+
+lurgy: {
+  title: "Lurgy",
+  movements: [
+    "Ring the Heavens",
+    "Slow Lurgy",
+    "Whirling Lurgy",
+    "Meta-Lurgy"
+  ],
+  score: "scores/thornock_neil_lurgy.pdf"
+},
+
+meditation: {
+  title: "Meditation",
+  score: "scores/thornock_neil_meditation.pdf"
+},
+
+musing: {
+  title: "Musing",
+  score: "scores/thornock_neil_musing.pdf"
+},
+
+nativity: {
+  title: "Nativity",
+  movements: [
+    "The Word Made Flesh",
+    "Shepherds",
+    "Angels",
+    "Joseph and Mary",
+    "Lullaby",
+    "Alleluia"
+  ],
+  score: "scores/thornock_neil_nativity.pdf"
+},
+
+nocturne: {
+  title: "Nocturne",
+  score: "scores/thornock_neil_nocturne.pdf"
+},
+
+octogoxary: {
+  title: "Octogoxary",
+  movements: ["Eight numbered movements"],
+  score: "scores/thornock_neil_octogoxary.pdf"
+},
+
+oldforms: {
+  title: "Old Forms",
+  movements: ["Ciaconna", "Recitative", "Toccata"],
+  score: "scores/thornock_neil_oldforms.pdf"
+},
+
+orb: {
+  title: "Orb",
+  score: "scores/thornock_neil_orb.pdf"
+},
+
+ouroboros: {
+  title: "Ouroboros",
+  movements: ["Circles", "Cycles"],
+  score: "scores/thornock_neil_ouroboros.pdf"
+},
+
+oval: {
+  title: "Oval",
+  score: "scores/thornock_neil_oval.pdf"
+},
+
+patriotictriptych: {
+  title: "Patriotic Triptych",
+  movements: [
+    "You’re a Grand Old Flag",
+    "When Johnny Comes Marching Home",
+    "Yankee Doodle"
+  ],
+  score: "scores/thornock_neil_patriotic_triptych.pdf"
+},
+
+phantasmagoriacamptownraces: {
+  title: "Phantasmagoria (Camptown Races)",
+  score: "scores/thornock_neil_phantasmagoria_camptown.pdf"
+},
+
+phantasmagoriapartita: {
+  title: "Phantasmagoria (Partita)",
+  score: "scores/thornock_neil_phantasmagoria_partita.pdf"
+},
+
+phantasmagoriawestminster: {
+  title: "Phantasmagoria (Westminster)",
+  score: "scores/thornock_neil_phantasmagoria_westminster.pdf"
+},
+
+polyjamitron: {
+  title: "Polyjamitron",
+  score: "scores/thornock_neil_polyjamitron.pdf"
+},
+
+preeloodiooz1: {
+  title: "Preeloodiooz 1",
+  score: "scores/thornock_neil_preeloodiooz1.pdf"
+},
+
+preeloodiooz2: {
+  title: "Preeloodiooz 2",
+  score: "scores/thornock_neil_preeloodiooz2.pdf"
+},
+
+preludeandchaconne: {
+  title: "Prelude and Chaconne",
+  score: "scores/thornock_neil_prelude_and_chaconne.pdf"
+},
+
+preludes: {
+  title: "Preludes",
+  movements: ["Three preludes"],
+  score: "scores/thornock_neil_preludes.pdf"
+},
+
+preludinos: {
+  title: "Preludinos",
+  movements: ["Three numbered movements"],
+  score: "scores/thornock_neil_preludinos.pdf"
+},
+
+quadmode: {
+  title: "Quadmode",
+  score: "scores/thornock_neil_quadmode.pdf"
+},
+
+raccoonrag: {
+  title: "Raccoon Rag",
+  movements: [
+    "Nocturnal Jaunt",
+    "Uncertain Encounter",
+    "Scurry"
+  ],
+  score: "scores/thornock_neil_raccoonrag.pdf"
+},
+
+reddance: {
+  title: "Red Dance"
+},
+
+redhorizan: {
+  title: "Red Horizan",
+  score: "scores/thornock_neil_red_horizon.pdf"
+},
+
+serenade: {
+  title: "Serenade",
+  score: "scores/thornock_neil_serenade.pdf"
+},
+
+slowdances: {
+  title: "Slow Dances",
+  movements: ["Three dances"],
+  score: "scores/thornock_neil_slowdances.pdf"
+},
+
+slowsofteven: {
+  title: "slow-soft-even",
+  movements: ["Three movements"],
+  score: "scores/thornock_neil_slowsofteven.pdf"
+},
+
+sonata3: {
+  title: "Sonata 3",
+  score: "scores/thornock_neil_sonata_3.pdf"
+},
+
+sonatinetypiqueatypique: {
+  title: "Sonatine Typique / Atypique",
+  movements: ["Six numbered movements"],
+  score: "scores/thornock_neil_sonatine_typique_atypique.pdf"
+},
+
+summerpieces: {
+  title: "Summer Pieces",
+  movements: [
+    "Rain Garden",
+    "The Sun Looks Down with a Golden Frown",
+    "The Jovial Moon Laughs and Dances",
+    "Daydream"
+  ],
+  score: "scores/thornock_neil_summerpieces.pdf"
+},
+
+threepieces: {
+  title: "Three Pieces",
+  score: "scores/thornock_neil_three-pieces.pdf"
+},
+
+threetunesfromfloraememoriae: {
+  title: "Three Tunes from Florae Memoriae",
+  score: "scores/thornock_neil_three_tunes_florae.pdf"
+},
+
+threeyankeetunesmithsongs: {
+  title: "Three Yankee Tunesmith Songs",
+  movements: ["Heroism", "When Jesus Wept", "Northfield"],
+  score: "scores/thornock_neil_three_yankee_tunes.pdf"
+},
+
+toccatagloriosa: {
+  title: "Toccata Gloriosa",
+  score: "scores/thornock_neil_toccata_gloriosa.pdf"
+},
+
+toccatina: {
+  title: "Toccatina",
+  score: "scores/thornock_neil_toccatina.pdf"
+},
+
+tokenmoon: {
+  title: "Token Moon",
+  score: "scores/thornock_neil_tokenmoon.pdf"
+},
+
+toytinker: {
+  title: "Toy Tinker",
+  score: "scores/thornock_neil_toy_tinker.pdf"
+},
+
+twilight: {
+  title: "Twilight",
+  movements: ["Four pieces"],
+  score: "scores/thornock_neil_twilight.pdf"
+},
+
+uncertainlandscape: {
+  title: "Uncertain Landscape",
+  movements: ["Grey Trees", "Blam!", "Legend"]
+},
+
+waltzes: {
+  title: "Waltzes",
+  movements: ["Five waltzes"],
+  score: "scores/thornock_neil_waltzes.pdf"
+},
+
+whimsicaldances: {
+  title: "Whimsical Dances",
+  movements: ["Five movements"],
+  score: "scores/thornock_neil_whimsical-dances.pdf"
+}
+
+};
